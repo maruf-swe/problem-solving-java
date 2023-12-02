312a6d514b35cf297e782bc104d2ec6546cba7dd
Marufur Rahman <59738629+maruf-swe@users.noreply.github.com>
2023-12-02 23:57:18 +0600

    Add stdin and stdout problem solution

---
+++ HackerRank/StdinAndStdout.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String str = scan.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + str);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
