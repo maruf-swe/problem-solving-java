b94dc1046adde71239df54ae48444ae7cb40d284
maruf-swe <marufur1847@gmail.com>
2023-12-05 23:58:04 +0600

    update in System.out.println line

--- HackerRank/LoopsOne.java
+++ HackerRank/LoopsOne.java
@@ -5,9 +5,9 @@ public class LoopsOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int i;
-        for (i = 1; i <= 10; i++) {
-            System.out.printf("%d * %d = %d", N, i, N*i).println();
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d", N, i, N*i);
+            System.out.println();
         }
     }
 }
